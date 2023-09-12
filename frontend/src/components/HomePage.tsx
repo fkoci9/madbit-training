b3cedbc31fed291662091737c1d35db2e4417e9e
fkoci-admin <f.koci@teamsystem.com>
2023-09-12 11:22:48 +0200

    HomePage and Comments fixed

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/store.tsx';
-import { editPost, removePost, addPost } from '../redux/postSlice.tsx';
+import { editPost, removePost, addPost , addNewPost } from '../redux/postSlice.tsx';
 import { logout } from "../redux/authSlice.tsx";
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
@@ -12,6 +12,9 @@ const HomePage: React.FC = () => {
     console.log(posts);
     const navigate = useNavigate();
     const [editingPostId, setEditingPostId] = useState<number | null>(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [newPost, setNewPost] = useState({ title: '', author: '' });
+    const [editedTitle, setEditedTitle] = useState('');
 
     useEffect(() => {
         if (posts.length === 0) {
@@ -19,17 +22,28 @@ const HomePage: React.FC = () => {
         }
     }, [dispatch, posts]);
 
+    const handleAddPost = () => {
+        dispatch(addNewPost(newPost));
+        setIsModalOpen(false); // Close the modal after adding a new post
+        setNewPost({ title: '', author: '' }); // Clear input fields
+    };
+
     const handleRemovePost = (postId: number) => {
         dispatch(removePost(postId));
     };
 
     const handleEditPost = (postId: number) => {
         setEditingPostId(postId);
+        // Set the edited title to the current post's title
+        const postToEdit = posts.find((post) => post.id === postId);
+        if (postToEdit) {
+            setEditedTitle(postToEdit.title);
+        }
     };
 
-    const handleSavePost = (postId: number, updatedContent: string) => {
-        dispatch(editPost({ postId, updatedContent }));
-        setEditingPostId(null);
+    const handleSavePost = (postId: number , editedTitle : string) => {
+        dispatch(editPost({ postId, updatedContent : editedTitle }));
+        setEditingPostId(null)
     };
 
     const handleCancelEdit = () => {
@@ -49,19 +63,63 @@ const HomePage: React.FC = () => {
         <div>
             <h1>Home Page</h1>
             <button onClick={handleLogout}>Log Out</button>
-
+            <button onClick={() => setIsModalOpen(true)}>Add New</button>
+            {isModalOpen && (
+                <div className="modal is-active">
+                    <div className="modal-background" onClick={() => setIsModalOpen(false)}></div>
+                    <div className="modal-content">
+                        <div className="box">
+                            <h2>Add New Post</h2>
+                            <div className="field">
+                                <label className="label">Title</label>
+                                <div className="control">
+                                    <input
+                                        className="input"
+                                        type="text"
+                                        placeholder="Enter title"
+                                        value={newPost.title}
+                                        onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+                                    />
+                                </div>
+                            </div>
+                            <div className="field">
+                                <label className="label">Author</label>
+                                <div className="control">
+                                    <input
+                                        className="input"
+                                        type="text"
+                                        placeholder="Enter author"
+                                        value={newPost.author}
+                                        onChange={(e) => setNewPost({ ...newPost, author: e.target.value })}
+                                    />
+                                </div>
+                            </div>
+                            <button className="button is-primary" onClick={handleAddPost}>
+                                Add Post
+                            </button>
+                        </div>
+                    </div>
+                    <button
+                        className="modal-close is-large"
+                        aria-label="close"
+                        onClick={() => setIsModalOpen(false)}
+                    ></button>
+                </div>
+            )}
             <ul>
                 {posts.map((post) => (
                     <li key={post.id}>
                         {currentUserType === 'admin' && editingPostId === post.id ? (
                             <>
                                 <div>
+                                    {/* Use the editedTitle in the input field */}
                                     <input
                                         type="text"
-                                        value={post.title}
-                                        onChange={(e) => handleSavePost(post.id, e.target.value)}
+                                        value={editedTitle}
+                                        onChange={(e) => setEditedTitle(e.target.value)}
                                     />
                                 </div>
+                                <button onClick={() => handleSavePost(post.id , editedTitle)}>Save</button>
                                 <button onClick={handleCancelEdit}>Cancel</button>
                             </>
                         ) : null}
