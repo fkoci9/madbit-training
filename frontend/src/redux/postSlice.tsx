33681cd71b3f00ae073b6ac3bd11b3de12975f84
fkoci-admin <f.koci@teamsystem.com>
2023-09-13 16:38:53 +0200

    fixing the reducer , making return statement and not pushing to the state

--- frontend/src/redux/postSlice.tsx
+++ frontend/src/redux/postSlice.tsx
@@ -1,132 +1,179 @@
-import { createSlice} from '@reduxjs/toolkit';
-
+import { createSlice } from "@reduxjs/toolkit";
 
 export interface Comment {
-    id: number;
-    postId : number;
-    title: string;
+  id: number;
+  postId: number;
+  title: string;
 }
 
 interface Post {
-    id: number;
-    title: string;
-    author: string;
-    comments: Comment[];
+  id: number;
+  title: string;
+  author: string;
+  comments: Comment[];
 }
-const dummyPosts:Post[] = [
-    {
-        id: 1,
-        title: 'Post 1',
-        author: 'Author 1',
-        comments: [{ id: 1, postId: 1 ,title: 'Comment 1 of Post 1' },
-            {id: 2 , postId: 1 ,title : 'Comment 2 of Post 1'}],
-    },
-    {
-        id: 2,
-        title: 'Post 2',
-        author: 'Author 2',
-        comments: [{ id: 2,postId: 2 , title: 'Comment 1 of Post 2' },
-            {id: 2 ,postId: 2 , title: 'Comment 2 of Post 2'}],
-    },
-    {
-        id: 3,
-        title: 'Post 3',
-        author: 'Author 3',
-        comments: [{ id: 3,postId: 3 , title: 'Comment 1 of Post 3' }],
-    },
-    {
-        id: 4,
-        title: 'Post 4',
-        author: 'Author 2',
-        comments: [{ id: 1, postId: 4 , title: 'Comment 1 of Post 4' },
-            {id: 2, postId: 4 , title: 'Comment 2 of Post 4'},
-            {id: 4, postId: 4 ,title: 'Comment 3 of Post 4'},
-            {id: 6,postId: 4 , title: 'Comment 4 of Post 4'}],
-    },
+
+const dummyPosts: Post[] = [
+  {
+    id: 1,
+    title: "Post 1",
+    author: "Author 1",
+    comments: [
+      { id: 1, postId: 1, title: "Comment 1 of Post 1" },
+      { id: 2, postId: 1, title: "Comment 2 of Post 1" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    author: "Author 2",
+    comments: [
+      { id: 2, postId: 2, title: "Comment 1 of Post 2" },
+      { id: 2, postId: 2, title: "Comment 2 of Post 2" },
+    ],
+  },
+  {
+    id: 3,
+    title: "Post 3",
+    author: "Author 3",
+    comments: [{ id: 3, postId: 3, title: "Comment 1 of Post 3" }],
+  },
+  {
+    id: 4,
+    title: "Post 4",
+    author: "Author 2",
+    comments: [
+      { id: 1, postId: 4, title: "Comment 1 of Post 4" },
+      { id: 2, postId: 4, title: "Comment 2 of Post 4" },
+      { id: 4, postId: 4, title: "Comment 3 of Post 4" },
+      { id: 6, postId: 4, title: "Comment 4 of Post 4" },
+    ],
+  },
 ];
 
 interface AppState {
-    posts: Post[];
+  posts: Post[];
 }
 
-const LOCAL_STORAGE_KEY = 'reduxState';
+const LOCAL_STORAGE_KEY = "reduxState";
 const storedState = localStorage.getItem(LOCAL_STORAGE_KEY);
-const initialState: AppState = storedState ? JSON.parse(storedState) : {
-    posts: dummyPosts,
-};
+const initialState: AppState = storedState
+  ? JSON.parse(storedState)
+  : {
+      posts: dummyPosts,
+    };
 
 const appSlice = createSlice({
-    name: 'app',
-    initialState,
-    reducers: {
+  name: "app",
+  initialState,
+  reducers: {
+    addNewPost: (state, action) => {
+      const newPost: Post = {
+        id: state.posts.length + 1,
+        title: action.payload.title,
+        author: action.payload.author,
+        comments: [],
+      };
+      const newState = {
+        ...state,
+        posts: [...state.posts, newPost],
+      };
 
-        addNewPost: (state, action) =>{
-            const newPost: Post = {
-                id: state.posts.length + 1,
-                title: action.payload.title,
-                author: action.payload.author,
-                comments: [],
-            };
-            state.posts.push(newPost);
-            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
-
-        },
-        removePost: (state, action) => {
-            const postId = action.payload;
-            state.posts = state.posts.filter(post => post.id !== postId);
-            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
-        },
-        editPost: (state, action) =>{
-            const { postId, updatedContent} = action.payload;
-            const post = state.posts.find((post) => post.id === postId);
-
-            if (post) {
-                const updatedPost = {
-                    ...post,
-                    title: updatedContent,
-                };
-
-                const index = state.posts.indexOf(post);
-                state.posts[index] =  updatedPost;
-                localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
-            }
-        },
-
-        addNewComment: (state, action) => {
-            const { postId, title } = action.payload;
-            const selectedPostIndex = state.posts.findIndex((post) => post.id === postId);
-
-            if (selectedPostIndex !== -1) {
-                const newComment: Comment = {
-                    id: state.posts[selectedPostIndex].comments.length + 1,
-                    postId : postId,
-                    title,
-                };
-
-                state.posts[selectedPostIndex].comments.push(newComment);
-                localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
-            }
-        },
-        removeComment: (state, action) => {
-            const { id, postId } = action.payload;
-            const selectedPostIndex = state.posts.findIndex((post) => post.id === postId);
-            state.posts[selectedPostIndex].comments = state.posts[selectedPostIndex].comments.filter(
-                (comment) => comment.id !== id
-            );
-            localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state));
-        },
-
-
-
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
+      return newState;
     },
+    removePost: (state, action) => {
+      const postId = action.payload;
+      const newState = {
+        ...state,
+        posts: state.posts.filter((post) => post.id !== postId),
+      };
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
+      return newState;
+    },
+    editPost: (state, action) => {
+      const { postId, updatedContent } = action.payload;
+      const post = state.posts.find((post) => post.id === postId);
+      if (post) {
+        const updatedPost = {
+          ...post,
+          title: updatedContent,
+        };
+        const index = state.posts.indexOf(post);
+        const newState = {
+          ...state,
+          posts: [
+            ...state.posts.slice(0, index),
+            updatedPost,
+            ...state.posts.slice(index + 1),
+          ],
+        };
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
+        return newState;
+      }
+    },
+    addNewComment: (state, action) => {
+      const { postId, title } = action.payload;
+      const selectedPostIndex = state.posts.findIndex(
+        (post) => post.id === postId,
+      );
+      if (selectedPostIndex !== -1) {
+        const newComment: Comment = {
+          id: state.posts[selectedPostIndex].comments.length + 1,
+          postId: postId,
+          title,
+        };
+        const updatedPost = {
+          ...state.posts[selectedPostIndex],
+          comments: [...state.posts[selectedPostIndex].comments, newComment],
+        };
+        const newState = {
+          ...state,
+          posts: [
+            ...state.posts.slice(0, selectedPostIndex),
+            updatedPost,
+            ...state.posts.slice(selectedPostIndex + 1),
+          ],
+        };
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
+        return newState;
+      }
+      return state;
+    },
+    removeComment: (state, action) => {
+      const { id, postId } = action.payload;
+      const selectedPostIndex = state.posts.findIndex(
+        (post) => post.id === postId,
+      );
+      if (selectedPostIndex !== -1) {
+        const updatedComments = state.posts[selectedPostIndex].comments.filter(
+          (comment) => comment.id !== id,
+        );
+        const updatedPost = {
+          ...state.posts[selectedPostIndex],
+          comments: updatedComments,
+        };
+        const newState = {
+          ...state,
+          posts: [
+            ...state.posts.slice(0, selectedPostIndex),
+            updatedPost,
+            ...state.posts.slice(selectedPostIndex + 1),
+          ],
+        };
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(newState));
+        return newState;
+      }
+      return state;
+    },
+  },
 });
 
 export const {
-
-    addNewPost,
-    removePost,
-    editPost,
-    addNewComment,
-    removeComment,
+  addNewPost,
+  removePost,
+  editPost,
+  addNewComment,
+  removeComment,
 } = appSlice.actions;
 export default appSlice.reducer;
