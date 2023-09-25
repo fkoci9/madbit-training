6a24a1cb907cd35d793b4b7774a347227e21d2b6
fkoci-admin <f.koci@teamsystem.com>
2023-09-25 15:20:08 +0200

    Remove useless file

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -1,134 +1,125 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { loginUser } from '../redux/authActions.tsx';
-import { useNavigate } from 'react-router-dom';
-import { TextField, Button , Box} from '@mui/material';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../redux/authSlice.tsx";
+import { useNavigate } from "react-router-dom";
+import { TextField, Button, Box } from "@mui/material";
+
 const LoginPage: React.FC = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [isRegistering, setIsRegistering] = useState(false);
-    const [registrationEmail, setRegistrationEmail] = useState('');
-    const [registrationPassword, setRegistrationPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    if (!localStorage.getItem('users')) {
-        const adminUser = {
-            email: '1',
-            password: '1',
-            type: 'admin',
-        };
-        localStorage.setItem('users', JSON.stringify([adminUser]));
-    }
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    registrationEmail: "",
+    registrationPassword: "",
+  });
 
-    const handleLogin = () => {
-        const storedUsers = JSON.parse(localStorage.getItem('users') || '[]');
-        console.log(storedUsers);
-        const user = storedUsers.find((u: any) => u.email === email && u.password === password);
+  const [error, setError] = useState("");
+  const [isRegistering, setIsRegistering] = useState(false);
 
-        if (user) {
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            // @ts-ignore
-            dispatch(loginUser());
-            navigate('/home');
-        } else {
-            setError('Invalid email or password');
-        }
+  const storedUsers = JSON.parse(localStorage.getItem("users") || "[]");
+
+  if (!localStorage.getItem("users")) {
+    const adminUser = {
+      email: "1",
+      password: "1",
+      type: "admin",
     };
-    const handleRegister = () => {
-        const storedUsers = JSON.parse(localStorage.getItem('users') || '[]');
+    localStorage.setItem("users", JSON.stringify([adminUser]));
+  }
 
-        const newUser = {
-            email: registrationEmail,
-            password: registrationPassword,
-            type: 'user',
-        };
-
-        storedUsers.push(newUser);
-        localStorage.setItem('currentUser', JSON.stringify(newUser));
-        localStorage.setItem('users', JSON.stringify(storedUsers));
-        setIsRegistering(false);
-        navigate('/home');
-    };
-
-    return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            minHeight="100vh"
-        >
-            <h1>{isRegistering ? 'Register' : 'Login'} Page</h1>
-            {isRegistering ? (
-                <Box
-                    display="flex"
-                    flexDirection="column"
-                    alignItems="center"
-                    justifyContent="center"
-                >
-                    <TextField
-                        type="email"
-                        label="Email"
-                        value={registrationEmail}
-                        onChange={(e) => setRegistrationEmail(e.target.value)}
-                        margin="normal"
-                    />
-                    <TextField
-                        type="password"
-                        label="Password"
-                        value={registrationPassword}
-                        onChange={(e) => setRegistrationPassword(e.target.value)}
-                        margin="normal"
-                    />
-                    <Button variant="contained" onClick={handleRegister} style={{ margin: '8px 0' }}>
-                        Register
-                    </Button>
-                    <Button
-                        variant="contained"
-                        onClick={() => setIsRegistering(false)}
-                        style={{ margin: '8px 0' }}
-                    >
-                        Cancel
-                    </Button>
-                </Box>
-            ) : (
-                <Box
-                    display="flex"
-                    flexDirection="column"
-                    alignItems="center"
-                    justifyContent="center"
-                >
-                    <TextField
-                        type="email"
-                        label="Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        margin="normal"
-                    />
-                    <TextField
-                        type="password"
-                        label="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        margin="normal"
-                    />
-                    <Button variant="contained" onClick={handleLogin} style={{ margin: '8px 0' }}>
-                        Login
-                    </Button>
-                    <Button
-                        variant="contained"
-                        onClick={() => setIsRegistering(true)}
-                        style={{ margin: '8px 0' }}
-                    >
-                        Register here
-                    </Button>
-                    {error && <p>{error}</p>}
-                </Box>
-            )}
-        </Box>
+  const handleLogin = () => {
+    const user = storedUsers.find(
+      (u: any) =>
+        u.email === formData.email && u.password === formData.password,
     );
+
+    if (user) {
+      localStorage.setItem("currentUser", JSON.stringify(user));
+      dispatch(login());
+      navigate("/home");
+    } else {
+      setError("Invalid email or password");
+    }
+  };
+
+  const handleRegister = () => {
+    const newUser = {
+      email: formData.registrationEmail,
+      password: formData.registrationPassword,
+      type: "user",
+    };
+
+    storedUsers.push(newUser);
+    localStorage.setItem("currentUser", JSON.stringify(newUser));
+    localStorage.setItem("users", JSON.stringify(storedUsers));
+    setIsRegistering(false);
+    navigate("/home");
+  };
+
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      minHeight="100vh"
+    >
+      <h1>{isRegistering ? "Register" : "Login"} Page</h1>
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <TextField
+          type="email"
+          label="Email"
+          value={isRegistering ? formData.registrationEmail : formData.email}
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              [isRegistering ? "registrationEmail" : "email"]: e.target.value,
+            })
+          }
+          margin="normal"
+        />
+        <TextField
+          type="password"
+          label="Password"
+          value={
+            isRegistering ? formData.registrationPassword : formData.password
+          }
+          onChange={(e) =>
+            setFormData({
+              ...formData,
+              [isRegistering ? "registrationPassword" : "password"]:
+                e.target.value,
+            })
+          }
+          margin="normal"
+        />
+        <Button
+          variant="contained"
+          onClick={isRegistering ? handleRegister : handleLogin}
+          style={{ margin: "8px 0" }}
+        >
+          {isRegistering ? "Register" : "Login"}
+        </Button>
+        {!isRegistering && (
+          <Button
+            variant="contained"
+            onClick={() => setIsRegistering(true)}
+            style={{ margin: "8px 0" }}
+          >
+            Register here
+          </Button>
+        )}
+        {error && <p>{error}</p>}
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
